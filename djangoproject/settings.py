ee37fd4ba0dda8bf8b8d4582c3df39ac98080dba
gokul-vscode <ramgokul637@gmail.com>
2025-12-18 21:21:28 +0530

    Add psycopg for postgres

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -87,20 +87,20 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES = {
- "default": dj_database_url.config(
-default=os.getenv("DATABASE_URL"),
-conn_max_age=600,
-ssl_require=True  # Render PostgreSQL requires SSL
-   	 )
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#  "default": dj_database_url.parse(
+# default=os.getenv("DATABASE_URL"),
+# conn_max_age=600,
+# ssl_require=True 
+#    	 )
 # }
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 # Password validation
