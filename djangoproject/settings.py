32483490e0c388695d2652b6d7514448d63d6b02
gokul-vscode <ramgokul637@gmail.com>
2025-12-18 19:03:21 +0530

    chnges

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
 import os
-
+import dj_database_url 
+from dotenv import load_dotenv
 load_dotenv()
+
+
 
 
 
@@ -33,9 +35,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
 
-ALLOWED_HOSTS = ['*']
 
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -86,13 +87,20 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ "default": dj_database_url.config(
+default=os.getenv("DATABASE_URL"),
+conn_max_age=600,
+ssl_require=True  # Render PostgreSQL requires SSL
+   	 )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
